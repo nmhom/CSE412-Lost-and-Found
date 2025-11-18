4fc9157c2730849341dbb5a006a2f5a6e796c185
Ishanshika Arora <ishanshikaarora@Ishanshikas-Laptop.local>
2025-11-17 23:27:46 -0700

    with documentation

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,26 +1,32 @@
 # backend/src/db.py
 import os, asyncpg
 from dotenv import load_dotenv
-load_dotenv(dotenv_path="backend/.env")
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-async def fetchrow(query: str, *params):
-    conn = await asyncpg.connect(DATABASE_URL)
+# load environment variables
+load_dotenv(dotenv_path="backend/.env")
+database_url = os.getenv("DATABASE_URL") # url mentioned in .env file
+
+# return a single row (like one user)
+async def fetchrow(query, *params):
+    conn = await asyncpg.connect(database_url)
     try:
         return await conn.fetchrow(query, *params)
     finally:
         await conn.close()
 
-async def fetch(query: str, *params):
-    conn = await asyncpg.connect(DATABASE_URL)
+# return multiple rows (like all items)
+async def fetch(query, *params):
+    conn = await asyncpg.connect(database_url)
     try:
         return await conn.fetch(query, *params)
     finally:
         await conn.close()
 
-async def execute(query: str, *params):
-    conn = await asyncpg.connect(DATABASE_URL)
+# doesn’t return rows (like INSERT, UPDATE, DELETE)
+async def execute(query, *params):
+    conn = await asyncpg.connect(database_url)
     try:
         return await conn.execute(query, *params)
     finally:
         await conn.close()
+

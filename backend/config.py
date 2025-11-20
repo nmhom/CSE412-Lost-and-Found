154a361155951c670e01457e628da3d762bb1d62
skohli182 <166069879+skohli182@users.noreply.github.com>
2025-11-20 12:47:21 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,4 @@ DATABASE_URL=""
 #here the formula is postgresql://username:poassword@localhost:5432/databasename
 #go into pgadmin on your lost and found database right click on the postgres above -> go to properties then connections and fill it out 
 #ex: postgresql://shaniakohli:password@localhost:5432/LostAndFound"
+#then to run it should be python3 app.py and you can get the link to the website, using the diferent app.routes you can visually see what is entered in the database
